c8bf08f067500077e3bded235383ad0dbcfbcbf7
vagrant <vagrant@homestead>
2022-01-05 16:13:17 +0000

    Update Array Access Methods

--- paapi5-php-sdk/src/com/amazon/paapi5/v1/GetItemsRequest.php
+++ paapi5-php-sdk/src/com/amazon/paapi5/v1/GetItemsRequest.php
@@ -560,7 +560,7 @@ class GetItemsRequest implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool 
     {
         return isset($this->container[$offset]);
     }
@@ -572,7 +572,7 @@ class GetItemsRequest implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed 
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -585,7 +585,7 @@ class GetItemsRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void 
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -601,7 +601,7 @@ class GetItemsRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void 
     {
         unset($this->container[$offset]);
     }
